960d6718647b24b4496c404ab798925caf00440b
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-06 08:40:48 -0400

    Create 242. Valid Anagram.java

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!map.containsKey(ch)) {
+                map.put(ch, 1);
+            } else {
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            Integer value = map.get(ch);
+            if (value == null) {
+                return false;
+            }
+            value = (value == 1) ? null : value - 1;
+            map.put(ch, value);
+        }
+        return true;
+    }
+}
