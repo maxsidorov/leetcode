1dcf5e024ac11a4be98ebc5f7cbf8511dae13f53
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-09 16:44:17 -0400

    Update 160. Intersection of Two Linked Lists.java

--- 160. Intersection of Two Linked Lists.java
+++ 160. Intersection of Two Linked Lists.java
@@ -43,3 +43,17 @@ public class Solution1 {
         return length;
     }
 }
+
+
+public class Solution2 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode currA = headA;
+        ListNode currB = headB;
+        while (currA != currB) {
+            currA = currA != null ? currA.next : headB;
+            currB = currB != null ? currB.next : headA;
+        }
+        return currA;
+    }
+}
