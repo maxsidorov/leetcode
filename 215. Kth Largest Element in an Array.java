c88c39e3e86a7459b04e14771a051fac09749a7a
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-16 16:04:23 -0400

    Update 215. Kth Largest Element in an Array.java

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -21,3 +21,35 @@ class Solution2 {
 }
 
 // 3 - Quick select Avr O(N) Worst O(N*N)
+class Solution3 {
+    public int findKthLargest(int[] nums, int k) {
+        return findKthLargestImpl(nums, k - 1, 0, nums.length - 1);
+    }
+    
+    private int findKthLargestImpl(int[] nums, int k, int left, int right) {
+        //System.out.println(Arrays.toString(nums));
+        int pivat = left + (right - left)/2;
+        swap(nums, pivat, right);
+        int middle = left;
+        for (int i = left; i < right; i++) {
+            if (nums[i] > nums[right]) {
+                swap(nums, i, middle++);
+            }
+        }
+        swap(nums, right, middle);
+        if (middle == k) {
+            return nums[middle];
+        }
+        if (k < middle) {
+            return findKthLargestImpl(nums, k, left, middle - 1);
+        } else {
+            return findKthLargestImpl(nums, k, middle + 1, right);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
