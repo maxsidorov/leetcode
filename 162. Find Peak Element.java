f6b3a4ac6568a784ed5f556d892c8769884049ba
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-16 16:26:05 -0400

    Create 162. Find Peak Element.java

---
+++ 162. Find Peak Element.java
@@ -0,0 +1,13 @@
+// O(N)
+class Solution1 {
+    public int findPeakElement(int[] nums) {
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i-1] < nums[i] && nums[i] > nums[i+1]) {
+                return i;
+            }
+        }
+        return nums[0] > nums[nums.length - 1] ? 0 : nums.length - 1;
+    }
+}
+
+// O(Log N)
