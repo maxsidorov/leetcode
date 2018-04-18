2bb1fca3ffaa774b485565785be57377a5e586cd
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-18 13:48:56 -0400

    Update 34. Search for a Range.java

--- 34. Search for a Range.java
+++ 34. Search for a Range.java
@@ -21,3 +21,33 @@ class Solution1 {
 }
 
 // O(LogN)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (nums.length == 0 || nums[left] != target) {
+            return new int[]{-1, -1};
+        }
+        int resultLeft = left;
+
+        left = 0;
+        right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1)/2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        
+        return new int[]{resultLeft, left};
+    }
+}
