aaad49dea2e56f38c435b55bd2254aad31275ee2
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-18 13:08:09 -0400

    Update 162. Find Peak Element.java

--- 162. Find Peak Element.java
+++ 162. Find Peak Element.java
@@ -11,3 +11,17 @@ class Solution1 {
 }
 
 // O(Log N)
+class Solution2 {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
