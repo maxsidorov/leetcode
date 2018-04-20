6a2dc702cfa091edd557a5cdae229b522fbba763
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-20 13:14:00 -0400

    Update 33. Search in Rotated Sorted Array.java

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -28,3 +28,38 @@ class Solution1 {
         return minIndex;
     }
 }
+
+// O(LogN) now
+class Solution2 {
+    public int search(int[] nums, int target) {
+        int N = nums.length;
+        if (N == 0) return -1;
+        int minIndex = getMinIndex(nums);
+        System.out.println(minIndex);
+        int left = minIndex, right = minIndex + nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right)/2;
+            if (nums[mid % N] == target) return mid % N;
+            if (nums[mid % N] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+        
+    }
+    
+    private int getMinIndex(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (nums[left] > nums[right]) {
+            int mid = (left + right)/2;
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
