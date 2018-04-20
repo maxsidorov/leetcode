e9e5b0ce6fc3b3efaa888a70acc6da196bef9d79
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-20 16:20:34 -0400

    Update 240. Search a 2D Matrix II.java

--- 240. Search a 2D Matrix II.java
+++ 240. Search a 2D Matrix II.java
@@ -20,3 +20,18 @@ class Solution1 {
 }
 
 // O(N+M)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int row = 0, col = matrix[0].length -1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
