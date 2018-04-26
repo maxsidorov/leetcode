ee6aa2c103968ed55953a1eb9f15459ac4d4fd8a
Max Sidorov <sidorovmaxim13@gmail.com>
2018-04-26 16:32:12 -0400

    Create 202. Happy Number.java

---
+++ 202. Happy Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            int sum = 0;
+            while (n > 0) {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
