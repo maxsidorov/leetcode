7f736fd7f3753c52a9bb1f55014e65ce17a27ca2
Max Sidorov <sidorovmaxim13@gmail.com>
2018-05-14 11:57:01 -0400

    Create 371. Sum of Two Integers.java

---
+++ 371. Sum of Two Integers.java
@@ -0,0 +1,13 @@
+// I didn't solve it by myself
+class Solution {
+    public int getSum(int a, int b) {
+        
+       while(b!=0){
+           int c = a&b;
+           a=a^b;
+           b=c<<1;
+       }
+
+       return a;        
+    }
+}
