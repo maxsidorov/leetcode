9b8ccef7514078c9d82ed1a3b8ed0492abbfa87c
Max Sidorov <sidorovmaxim13@gmail.com>
2018-05-23 14:41:21 -0400

    Create 287. Find the Duplicate Number.java

---
+++ 287. Find the Duplicate Number.java
@@ -0,0 +1,17 @@
+// I didn't solve it by myself!!! Too tricky
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[slow];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
