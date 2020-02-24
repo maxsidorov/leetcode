eba2b0d3392560e1303495339d6ffab46ad96699
Max Sidorov <sidorovmaxim13@gmail.com>
2020-02-24 13:44:28 -0500

    Update 91. Decode Ways.java

--- 91. Decode Ways.java
+++ 91. Decode Ways.java
@@ -16,3 +16,22 @@ class Solution {
 
   }
 }
+
+// My DP solution
+class Solution {
+  public int numDecodings(String s) {
+    if (s.charAt(0) == '0') return 0;
+    int[] dp = new int[s.length() + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) != '0') {
+        dp[i + 1] = dp[i];
+      }
+      if (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) >= '0' && s.charAt(i) <= '6')) {
+        dp[i + 1] += dp[i - 1];
+      }
+    }
+    return dp[s.length()];
+  }
+}
